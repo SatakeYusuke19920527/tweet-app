9f7a19193d56a04a84fa1b349f0ec2e9aedebf59
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-04 07:39:46 +0900

    add: firebase connect

---
+++ src/util/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import "firebase/firestore"
+
+const {
+  REACT_APP_FIREBASE_APIKEY,
+  REACT_APP_FIREBASE_AUTHDOMAIN,
+  REACT_APP_FIREBASE_DATABASEURL,
+  REACT_APP_FIREBASE_PROJECTID,
+  REACT_APP_FIREBASE_STORAGEBUCKET,
+  REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  REACT_APP_FIREBASE_APPID
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_APIKEY,
+  authDomain: REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASEURL,
+  projectId: REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: REACT_APP_FIREBASE_APPID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
